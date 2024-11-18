cf1dbddc43b792eabf6de67464335e9f87f9aac0
bhavesh <bhaveshpamnani100@gmail.com>
2024-11-18 17:41:13 +0530

    some changes

--- server/routes/wishlistRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const routes = express.Router();
-const {toggleProduct,getWishlistStatus,getProductsByUser} = require("../controller/wishlistController");
-
-routes.post("/toggle",toggleProduct);
-routes.get('/status/:userId/:productId', getWishlistStatus);
-routes.get('/product-by-user/:userId', getProductsByUser);
-
-module.exports = routes;
